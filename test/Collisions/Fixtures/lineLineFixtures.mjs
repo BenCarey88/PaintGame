353f95dc1dc510b013494f55c9f80eac25f3c0a2
Ben Carey <Ben Carey>
2020-03-19 16:07:57 +0000

    yh boi completed all collision tests

--- test/Collisions/Fixtures/lineLineFixtures.mjs
+++ test/Collisions/Fixtures/lineLineFixtures.mjs
@@ -7,6 +7,7 @@ export var line_line = {
     FIXTURE_HEIGHT: 130
 };
 
+//standard collision
 line_line.collision = {
 
     line1: new Line(
@@ -16,22 +17,149 @@ line_line.collision = {
         new Vector(50, 150), new Vector(180, 80)
     ),
     line2: new Line(
-        new Vector(40, 80), new Vector(180, 100), 5
+        new Vector(50, 160), new Vector(180, 90)
     ),
     rLine2: new Line(
-        new Vector(180, 100), new Vector(40, 80), 5
+        new Vector(50, 160), new Vector(180, 90)
     ),
     line3: new Line(
-        new Vector(140, 50), new Vector(140, 130), 10
+        new Vector(40, 80), new Vector(170, 100), 5
     ),
     rLine3: new Line(
-        new Vector(140, 130), new Vector(140, 50), 10
+        new Vector(170, 100), new Vector(40, 80), 5
     ),
     line4: new Line(
-        new Vector(40, 40), new Vector(190, 140), 5
+        new Vector(140, 50), new Vector(140, 130), 10
     ),
     rLine4: new Line(
+        new Vector(140, 130), new Vector(140, 50), 10
+    ),
+    line5: new Line(
+        new Vector(40, 40), new Vector(190, 140), 5
+    ),
+    rLine5: new Line(
         new Vector(190, 140), new Vector(40, 40), 5
+    ),
+    line6: new Line(
+        new Vector(135, 125), new Vector(120, 98), 10
+    ),
+    rLine6: new Line(
+        new Vector(120, 98), new Vector(135, 125), 10
+    ),
+
+    FIXTURE_LENGTH: 160,
+    FIXTURE_TRANSFORM: new Vector(-20, -20),
+    FIXTURE_SCALE: 0.9,
+}
+
+//boundary case
+line_line.boundary_case = {
+    line1: new Line(
+        new Vector(60, 60), new Vector(160, 60), 40
+    ),
+    rLine1: new Line(
+        new Vector(160, 60), new Vector(60, 60), 40
+    ),
+
+    line2: new Line(
+        new Vector(25, 65), new Vector(55, 25), 10
+    ),
+    rLine2: new Line(
+        new Vector(55, 25), new Vector(25, 65), 10
+    ),
+    line3: new Line(
+        new Vector(90, 30), new Vector(160, 30), 20
+    ),
+    rLine3: new Line(
+        new Vector(160, 30), new Vector(90, 30), 20
+    ),
+    line4: new Line(
+        new Vector(185, 60), new Vector(200, 100), 10
+    ),
+    rLine4: new Line(
+        new Vector(200, 100), new Vector(185, 60), 10
+    ),
+    line5: new Line(
+        new Vector(37.5, 30), new Vector(37.5, 110), 5
+    ),
+    rLine5: new Line(
+        new Vector(37.5, 110), new Vector(37.5, 30), 5
+    ),
+    line6: new Line(
+        new Vector(80, 90), new Vector(150, 110), 20
+    ),
+    rLine6: new Line(
+        new Vector(150, 110), new Vector(80, 90), 20
+    ),
+
+    FIXTURE_LENGTH: 200,
+    FIXTURE_TRANSFORM: new Vector(0, 10),
+    FIXTURE_SCALE: 0.9,
+}
+
+//fully_inside
+line_line.fully_inside = {
+    line1: new Line(
+        new Vector(60, 50), new Vector(160, 70), 70
+    ),
+    rLine1: new Line(
+        new Vector(160, 70), new Vector(60, 50), 70
+    ),
+    line2: new Line(
+        new Vector(60, 50), new Vector(160, 70), 50
+    ),
+    rLine2: new Line(
+        new Vector(160, 70), new Vector(60, 50), 50
+    ),
+    line3: new Line(
+        new Vector(50, 60), new Vector(80, 40), 5
+    ),
+    rLine3: new Line(
+        new Vector(80, 40), new Vector(50, 60), 5
+    ),
+
+    FIXTURE_LENGTH: 200,
+    FIXTURE_TRANSFORM: new Vector(0, 10),
+    FIXTURE_SCALE: 0.9,
+}
+
+//no collision
+line_line.no_collision = {
+    line1: new Line(
+        new Vector(50, 150), new Vector(180, 130)
+    ),
+    rLine1: new Line(
+        new Vector(180, 130), new Vector(50, 150)
+    ),
+    line2: new Line(
+        new Vector(40, 80), new Vector(60, 130), 5
+    ),
+    rLine2: new Line(
+        new Vector(60, 130), new Vector(40, 80), 5
+    ),
+    line3: new Line(
+        new Vector(140, 50), new Vector(140, 120), 10
+    ),
+    rLine3: new Line(
+        new Vector(140, 120), new Vector(140, 50), 10
+    ),
+    line4: new Line(
+        new Vector(40, 40), new Vector(130, 100), 5
+    ),
+    rLine4: new Line(
+        new Vector(130, 100), new Vector(40, 40), 5
+    ),
+    line5: new Line(
+        new Vector(155, 50), new Vector(155, 110), 10
+    ),
+    rLine5: new Line(
+        new Vector(155, 110), new Vector(155, 50), 10
+    ),
+    line6: new Line(
+        new Vector(65, 100), new Vector(90, 90)
+    ),
+    rLine6: new Line(
+        new Vector(90, 90), new Vector(65, 100)
     ),
 
     FIXTURE_LENGTH: 160,
--- test/Collisions/LineLineCollisions/lineLineCollisions.mjs
+++ test/Collisions/LineLineCollisions/lineLineCollisions.mjs
@@ -24,4 +24,81 @@ export class LineLineCollisions extends Tests {
 		);
 	}
 
+	test_fully_inside() {
+		this._all_lines_collide(
+			this.fixture.fully_inside
+		);
+	}
+
+	test_boundary_case() {
+		var line1 = [
+			this.fixture.boundary_case.line1,
+			this.fixture.boundary_case.rLine1,
+		];
+		var lines = [
+			this.fixture.boundary_case.line3,
+			this.fixture.boundary_case.rLine3,
+			this.fixture.boundary_case.line4,
+			this.fixture.boundary_case.rLine4,
+			this.fixture.boundary_case.line5,
+			this.fixture.boundary_case.rLine5,
+			this.fixture.boundary_case.line6,
+			this.fixture.boundary_case.rLine6,
+		];
+		for (var l1 of line1) {
+			for (var l2 of lines) {
+				this.assertTrue(
+					collisionLineLine(l1, l2)
+				);
+				this.assertTrue(
+					collisionLineLine(l2, l1)
+				);
+			}
+		}
+	}
+
+	test_no_collision() {
+		var lines = {
+			line1: [
+				this.fixture.no_collision.line1,
+				this.fixture.no_collision.rLine1,
+			],
+			line2: [
+				this.fixture.no_collision.line2,
+				this.fixture.no_collision.rLine2,
+			],
+			line3: [
+				this.fixture.no_collision.line3,
+				this.fixture.no_collision.rLine3,
+			],
+			line4: [
+				this.fixture.no_collision.line4,
+				this.fixture.no_collision.rLine4,
+			],
+			line5: [
+				this.fixture.no_collision.line5,
+				this.fixture.no_collision.rLine5,
+			],
+			line6: [
+				this.fixture.no_collision.line6,
+				this.fixture.no_collision.rLine6,
+			],
+		}
+		// compare each element of one array with each element
+		// from the other arrays
+		for (var line1 in lines) {
+			for (var line2 in lines) {
+				if (line2 != line1) {
+					var lineList1 = lines[line1];
+					var lineList2 = lines[line2];
+					for (var l1 of lineList1) {
+						for (var l2 of lineList2) {
+							this.assertFalse(collisionLineLine(l1, l2));
+						}
+					}
+				}
+			}
+		}
+	}
+
 }
